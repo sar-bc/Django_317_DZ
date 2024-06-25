7b5bf09349cee0425b0ca7a5d93c8dcd845524d3
Виктор <sar-bc@mail.ru>
2024-06-26 02:10:14 +0400

    dz_95

--- dz_95_site_about_the_city_registration_user/mysite/city/urls.py
+++ dz_95_site_about_the_city_registration_user/mysite/city/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('show_post/<int:id>/', views.show_post, name='show_post'),
+    path('registration/', views.registration, name='registration'),
+    path('logoutuser/', views.logoutuser, name='logoutuser'),
+    path('loginuser/', views.loginuser, name='loginuser'),
 
 ]
--- dz_95_site_about_the_city_registration_user/mysite/city/views.py
+++ dz_95_site_about_the_city_registration_user/mysite/city/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db import IntegrityError
 from city.models import Posts
 
 
@@ -19,3 +22,41 @@ def show_post(request, id):
     post.views += 1
     post.save(update_fields=['views'])
     return render(request, 'city/show_post.html', {'post': post})
+
+
+def registration(request):
+    if request.method == "GET":
+        return render(request, 'city/registration.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'city/registration.html',
+                              {'form': UserCreationForm(),
+                               'error': 'Такое имя пользователя уже существует. Задайте другое'})
+        else:
+            return render(request, 'city/registration.html',
+                          {'form': UserCreationForm(),
+                           'error': 'Пароли не совпадают'})
+
+def logoutuser(request):
+    # if request.method == "POST":
+    logout(request)
+    return redirect('home')
+
+def loginuser(request):
+    if request.method == "GET":
+        return render(request, 'city/login.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'city/login.html',
+                          {'form': AuthenticationForm(),
+                           'error': 'Неверные данные'})
+        else:
+            login(request, user)
+            return redirect('home')
